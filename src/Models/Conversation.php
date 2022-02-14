db7be941eff12a42c2e3d3c3f68feb4c2a99448d
Adinnu Benedict <benidixt@gmail.com>
2022-02-14 15:54:18 +0100

    Reduce Query

--- src/Models/Conversation.php
+++ src/Models/Conversation.php
@@ -71,7 +71,7 @@ class Conversation extends BaseModel
      */
     public function messages()
     {
-        return $this->hasMany(Message::class, 'conversation_id'); //->with('sender');
+        return $this->hasMany(Message::class, 'conversation_id')->with('participation');
     }
 
     /**
